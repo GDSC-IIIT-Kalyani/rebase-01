8387fcefa5cd3b544bc5d43472956dc6089e8190
shashwatdwi176 <dwivedishashwat176@gmail.com>
2023-11-29 20:04:14 +0530

    Added Footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import './globals.css'
 import About from '@/app/about'
+import Footer from '@/app/footer'
 
 export default function Home() {
   return (
     <div className=''>
       
       <About/>
+      <Footer />
     </div>
   )
 }
