94fa177189ac8503200cfef8bf5983578745d518
shashwatdwi176 <dwivedishashwat176@gmail.com>
2023-11-29 19:13:36 +0530

    fixed minor bugs

--- src/app/about.tsx
+++ src/app/about.tsx
@@ -6,17 +6,15 @@ export default function About() {
     <>
      
     <div className="bg-[#F4E1B9] min-h-screen">
-        <div className='min-w-screen p=[40%]'>
-      <p className="font-display text-5xl text-red-500 stroke-current stroke-2 font-extrabold text-left">
-        About
-      </p>
+        <div className='min-w-screen pl-[13%] pt-2 '>
+        <Image src="/About.svg" alt="about box" width={400} height={100}  />
       <div className="flex flex-row">
         <div className="relative">
-          <Image src="/aboutbox.svg" alt="about box" width={700} height={500} />
+          <Image src="/aboutbox.svg" alt="about box" width={700} height={500} className='translate-y-[-10px]' />
 
-          <div className="absolute inset-0 flex items-center justify-center">
-            <p className="text-white text-lg">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque amet sed corporis sequi veritatis inventore a ratione quas atque unde. Laborum praesentium reprehenderit assumenda eaque, possimus beatae cumque officiis velit eligendi, neque voluptatibus necessitatibus dolorum itaque minus nostrum aperiam saepe. Ex qui a, incidunt harum vitae rem officia repudiandae nam, eveniet sapiente excepturi quia animi illum eius facilis amet magnam sed. Accusamus ad veritatis officia?
+          <div className="absolute inset-0 flex items-center justify-center w-[30rem] h-[3rem] translate-y-72 translate-x-8">
+            <p className="text-white text-lg text-justify ">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga explicabo nemo dicta beatae, officia officiis numquam dolores, expedita fugit culpa quia doloremque vel sequi! Quam magnam provident dolorem amet, iure ea odio officiis repudiandae maxime vel! Officia quos fugiat commodi! Ducimus maxime, veritatis hic modi aut doloribus doloremque velit voluptas quis laudantium incidunt asperiores eum rerum, placeat accusamus aperiam adipisci, explicabo architecto ipsam ab fugit. Voluptatibus, quibusdam suscipit exercitationem incidunt veniam eius vitae temporibus! Sint doloribus sunt totam esse sed.
             </p>
           </div>
         </div>
