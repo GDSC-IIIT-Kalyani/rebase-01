65c64552ca20ebfa26a97773cd55569fedf18235
TeeWrath <subroto.2003@gmail.com>
2023-11-30 20:32:31 +0530

    feat: landing page for desktops

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,32 +4,44 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <section className='hero'>
-      <div className='hero-text flex gap-10'>
+      <div className='hero-text flex relative'>
+        <div className='translate-x-[46rem] flex gap-10'>
         <div className='hero-heading'>
-          <h1 className='text-9xl text-red-600 text-stroke'>R e b a s e</h1>
+          <h1 className='text-9xl text-red-600 text-stroke'>Rebase</h1>
         </div>
-        <div className='hero-vol'>
-          <h3>
+        <div className='hero-vol translate-y-12'>
+          <h3 className='font-extrabold text-xl'>
             Volume
           </h3>
         </div>
-        {/* <Image src="/01.svg" alt="01" width={400} height={400} className='float-right' /> */}
-      </div>
-      
-      {/* Content */}
-      <div className='hero-content flex justify-center'>
-      {/* Left side stuff */}
-      <div className='left translate-x-16'>
-        <Image src="/Leftbox.svg" alt='' width={285} height={500} className='' />
+        </div>
+        <div>
+          <Image src="/01.svg" alt="01" width={600} height={600} className='absolute right-64 top-8 z-20' />
+        </div>
       </div>
 
-      {/* Right side stuff */}
-      <div className='right'>
-        <div className='relative'>
-          <Image src="/heroimage.svg" alt='' width={1400} height={600} className='-translate-y-[71px] -translate-x-16' />
-        </div>
+      {/* Content */}
+      <div className='hero-content flex justify-center items-center'>
         
-      </div>
+        {/* Left side stuff */}
+        <div className='left translate-x-20 relative'>
+          <Image src="/Leftbox.svg" alt='' width={285} height={500} className='' />
+          <ul className='block absolute top-16 left-4 content-center translate-x-5'>
+            <li><a href="https://gdsc.bio.link/"><Image src="/gdschero.svg" alt='' width={200} height={100} className='' /></a></li>
+            <li><a href="https://discord.gg/tcaxPN6CNs"><Image src="/discordhero.svg" alt='' width={100} height={100} className='ml-12 my-8' /></a></li>
+            <li><a href="https://twitter.com/gdsciiitkalyani"><Image src="/x.svg" alt='' width={60} height={100} className='ml-16 my-8' /></a></li>
+            <li><a href="https://www.youtube.com/channel/UCvRPKamvUtnQhij5y1MF11A"><Image src="/ythero.svg" alt='' width={70} height={100} className='ml-[3.5rem] my-8' /></a></li>
+            <li><a href="https://instagram.com/gdsciiitkalyani"><Image src="/instahero.svg" alt='' width={50} height={100} className='ml-16 my-8' /></a></li>
+          </ul>
+        </div>
+
+        {/* Right side stuff */}
+        <div className='right'>
+          <div className='relative'>
+            <Image src="/heroimage.svg" alt='' width={1400} height={600} className='-translate-y-[28px] -translate-x-16' />
+          </div>
+
+        </div>
       </div>
     </section>
   )
