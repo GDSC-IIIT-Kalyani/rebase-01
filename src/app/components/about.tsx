0c10c8dd2cf70218ca13cdf5e3a4971268040a4c
OindrilaDas27 <oindrila.das2708@gmail.com>
2023-12-14 00:34:28 +0530

    update about page

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,13 +1,14 @@
 import '../globals.css';
 import Image from "next/image"
-import { Bangers, KosugiMaru } from '../fonts';
+import { Bangers, JapaneseRobot, KosugiMaru } from '../fonts';
 
 export default function About() {
   return (
     <>
       <div className="bg-[#F4E1B9] min-h-screen">
         <div className='min-w-screen pl-[13%] pt-2 '>
-          <div className="faq-heading text-[12rem] relative z-10 text-[#E82338] text-stroke-2 translate-y-28 translate-x-32">
+          <div className='h-40 w-[34%] relative z-0 translate-y-[18rem] translate-x-[4rem] mt-[-16rem] border-t-[5px] border-l-[5px] border-black' ></div>
+          <div className="faq-heading text-[12rem] relative z-10 text-[#E82338] text-stroke-2 translate-y-[6rem] translate-x-32">
             <h1 className={Bangers.className} >ABOUT</h1>
           </div>
           <div className="flex flex-row">
@@ -16,11 +17,17 @@ export default function About() {
 
               <div className="absolute inset-0 flex items-center justify-center w-[30rem] h-[3rem] translate-y-72 translate-x-32 text-white text-xl text-justify font-bold">
                 <p className={KosugiMaru.className}>
-                Unleash your creativity and be part of Rebase{'<01>'}, a month-long online hackathon extravaganza! Compete across various categories, showcasing your skills, creativity, and teamwork alongside peers who share your passion. This is your chance to challenge yourself, learn new skills, and create remarkable projects that can make a real difference. {"Don't"} miss this unparalleled opportunity to rebase your talent and elevate your skills to a new level. Step into Rebase{'<'}01{'>'}—where creativity has no bounds.
+                  Unleash your creativity and be part of Rebase{'<01>'}, a month-long online hackathon extravaganza! Compete across various categories, showcasing your skills, creativity, and teamwork alongside peers who share your passion. This is your chance to challenge yourself, learn new skills, and create remarkable projects that can make a real difference. {"Don't"} miss this unparalleled opportunity to rebase your talent and elevate your skills to a new level. Step into Rebase{'<'}01{'>'}—where creativity has no bounds.
                 </p>
               </div>
             </div>
-            <Image src="/Computer.svg" alt="computer" width={700} height={500} className='translate-x-[-2rem]' />
+            <div className='flex flex-column justify-items-end'>
+              <div className='translate-y-[-8rem] translate-x-[35rem] jutify-self-end text-6xl text-[#e32735]'>
+                <span className={JapaneseRobot.className}>01.</span>
+              </div>
+              <Image src="/Computer.svg" alt="computer" width={700} height={500} className='translate-x-[-8rem] translate-y-[-4rem]' />
+            </div>
+
           </div>
         </div>
       </div>
