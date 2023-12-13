87a18e6bc2d05e071d0424199218cc03f75d9687
OindrilaDas27 <oindrila.das2708@gmail.com>
2023-12-13 23:54:00 +0530

    update about page

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,27 +1,26 @@
 import '../globals.css';
 import Image from "next/image"
-import { Bangers } from '../fonts';
+import { Bangers, KosugiMaru } from '../fonts';
 
 export default function About() {
   return (
     <>
-
       <div className="bg-[#F4E1B9] min-h-screen">
         <div className='min-w-screen pl-[13%] pt-2 '>
-          <div className="faq-heading text-[12rem] text-[#E82338] text-stroke-2 translate-y-24">
+          <div className="faq-heading text-[12rem] relative z-10 text-[#E82338] text-stroke-2 translate-y-28 translate-x-32">
             <h1 className={Bangers.className} >ABOUT</h1>
           </div>
           <div className="flex flex-row">
             <div className="relative">
-              <Image src="/aboutbox.svg" alt="about box" width={700} height={500} className='translate-y-[-10px]' />
+              <Image src="/aboutbox.svg" alt="about box" width={700} height={500} className='translate-y-[-1rem] translate-x-16' />
 
-              <div className="absolute inset-0 flex items-center justify-center w-[30rem] h-[3rem] translate-y-72 translate-x-8">
-                <p className="text-white text-lg text-justify ">
-                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga explicabo nemo dicta beatae, officia officiis numquam dolores, expedita fugit culpa quia doloremque vel sequi! Quam magnam provident dolorem amet, iure ea odio officiis repudiandae maxime vel! Officia quos fugiat commodi! Ducimus maxime, veritatis hic modi aut doloribus doloremque velit voluptas quis laudantium incidunt asperiores eum rerum, placeat accusamus aperiam adipisci, explicabo architecto ipsam ab fugit. Voluptatibus, quibusdam suscipit exercitationem incidunt veniam eius vitae temporibus! Sint doloribus sunt totam esse sed.
+              <div className="absolute inset-0 flex items-center justify-center w-[30rem] h-[3rem] translate-y-72 translate-x-32 text-white text-xl text-justify font-bold">
+                <p className={KosugiMaru.className}>
+                Unleash your creativity and be part of Rebase{'<01>'}, a month-long online hackathon extravaganza! Compete across various categories, showcasing your skills, creativity, and teamwork alongside peers who share your passion. This is your chance to challenge yourself, learn new skills, and create remarkable projects that can make a real difference. {"Don't"} miss this unparalleled opportunity to rebase your talent and elevate your skills to a new level. Step into Rebase{'<'}01{'>'}—where creativity has no bounds.
                 </p>
               </div>
             </div>
-            <Image src="/Computer.svg" alt="computer" width={700} height={500} />
+            <Image src="/Computer.svg" alt="computer" width={700} height={500} className='translate-x-[-2rem]' />
           </div>
         </div>
       </div>
