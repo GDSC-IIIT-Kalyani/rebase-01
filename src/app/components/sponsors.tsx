79ddd7bc1b5254cc401ef12a916c561fce4c5e52
Bhaskar Metiya <bhaskarmetiya@gmail.com>
2023-12-20 21:37:37 +0530

    fix: comment out unused tiers

--- src/app/components/sponsors.tsx
+++ src/app/components/sponsors.tsx
@@ -10,9 +10,9 @@ const Sponsors = () => {
                     <h1 className={Bangers.className}>Sponsors</h1>
                 </div>
                 <div className="wrapper flex flex-col gap-20" >
-                    <div className='flex relative bg-[#0F283F] h-48 w-[60%] border-black border-8'>
+                    {/* <div className='flex relative bg-[#0F283F] h-48 w-[60%] border-black border-8'>
                         <div className='sponsor-tier bg-[#59A3BC] w-48 h-12 absolute top-1/2 -right-40 outline-black outline-4 outline-offset-4 border-4 border-black outline grid place-items-center' style={AnimeAceBBbold.style}>PLATINUM</div>
-                    </div>
+                    </div> */}
                     <div className='flex justify-center items-center gap-20 relative bg-[#0F283F] h-48 w-[60%] border-black border-8 self-end'>
                         <div className="relative w-48 h-16">
                             <Image src='/Devfolio.png' layout='fill' alt='DEVFOLIO LOGO'></Image>
@@ -28,9 +28,9 @@ const Sponsors = () => {
                         </div>
                         <div className='sponsor-tier bg-[#FFAD00] w-48 h-12 absolute top-1/2 -right-40 outline-black outline-4 outline-offset-4 border-4 border-black outline grid place-items-center' style={AnimeAceBBbold.style}>GOLD</div>
                     </div>
-                    <div className='flex relative bg-[#0F283F] h-48 w-[60%] border-black border-8 self-end'>
+                    {/* <div className='flex relative bg-[#0F283F] h-48 w-[60%] border-black border-8 self-end'>
                         <div className='sponsor-tier bg-[#A4CF48] w-48 h-12 absolute top-1/2 -left-40 outline-black outline-4 outline-offset-4 border-4 border-black outline grid place-items-center' style={AnimeAceBBbold.style}>SILVER</div>
-                    </div>
+                    </div> */}
                 </div>
             </div>
 
