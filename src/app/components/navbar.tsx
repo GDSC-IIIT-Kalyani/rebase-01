ab174ce6850a458c25c5e497bdb5ca891c5838e7
TeeWrath <subroto.2003@gmail.com>
2023-12-21 18:23:41 +0530

    fix: navbar links

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -9,12 +9,12 @@ const navbar = () => {
             <div className='flex justify-center p-2'>
                 <nav className=''>
                     <ul className='flex 2xl:gap-28 xl:gap-20 lg:gap-16 md:gap-12 2xl:text-xl xl:text-lg lg:text-sm md:text-xs font-bold 2xl:py-6 md:py-4 2xl:px-16 md:px-8 xl:px-12 bg-[#de533c] '>
-                        <li><a href="#" className={AnimeAceBBbold.className}>About</a></li>
-                        <li><a href="#" className={AnimeAceBBbold.className} >Schedule</a></li>
-                        <li><a href="#" className={AnimeAceBBbold.className} >Tracks</a></li>
-                        <li><a href="#" className={AnimeAceBBbold.className} >Prizes</a></li>
-                        <li><a href="#" className={AnimeAceBBbold.className} >Sponsors</a></li>
-                        <li><a href="#" className={AnimeAceBBbold.className} >FAQs</a></li>
+                        <li className='z-20' ><a href="#" className={AnimeAceBBbold.className}>About</a></li>
+                        <li className='z-20' ><a href="#" className={AnimeAceBBbold.className} >Schedule</a></li>
+                        <li className='z-20' ><a href="#" className={AnimeAceBBbold.className} >Tracks</a></li>
+                        <li className='z-20' ><a href="#" className={AnimeAceBBbold.className} >Prizes</a></li>
+                        <li className='z-20' ><a href="#" className={AnimeAceBBbold.className} >Sponsors</a></li>
+                        <li className='z-20' ><a href="#" className={AnimeAceBBbold.className} >FAQs</a></li>
                     </ul>
                 </nav>
             </div>
