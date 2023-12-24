70262dcdef86b6f9ae9077ad6ad37866180509e3
Bhaskar Metiya <bhaskarmetiya@gmail.com>
2023-12-24 14:14:37 +0530

    feat: coming soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,10 @@
 import './globals.css'
-import Navbar from '@/app/components/navbar'
-import Faq from '@/app/components/faq'
-import Landing from '@/app/components/landing'
-import About from './components/about'
-import Timeline from './components/timeline'
-import Footer from '@/app/components/footer'
-import Tracks from './components/tracks'
-import Prizes from './components/prizes'
-import Sponsors from './components/sponsors'
+import ComingSoon from './components/comingSoon'
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <Landing />
-      <About />
-      <Timeline />
-      <Tracks />
-      <Prizes />
-      <Sponsors />
-      <Faq />
-      <Footer />
+      <ComingSoon />
     </>
   )
 }
