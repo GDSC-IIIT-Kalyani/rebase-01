735bb2ef1cb9cb8f73190d591b70ee346a8f7f16
Bhaskar Metiya <bhaskarmetiya@gmail.com>
2024-01-13 23:39:48 +0530

    feat: faqs + tailwind-fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      'bangers': ['Bangers'],
+      'kosugi': ['Kosugi\\ Maru'],
+    },
     extend: {
       screens: {
         extrasmall: { max: '639px' },
